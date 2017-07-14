96c75e6d680d68300fd1c4416af0368a3d9b1871
David Miguel <david.miguel@peaks.com>
2017-07-14 10:32:48 +0200

    Add api

---
+++ docs/reference/lists.js
@@ -0,0 +1,26 @@
+var DATA = [
+      { id:0, label:"com.google.android.youtube.player", link:"reference/com/google/android/youtube/player/package-summary.html", type:"package", deprecated:"false" },
+      { id:1, label:"com.google.android.youtube.player.YouTubeApiServiceUtil", link:"reference/com/google/android/youtube/player/YouTubeApiServiceUtil.html", type:"class", deprecated:"false" },
+      { id:2, label:"com.google.android.youtube.player.YouTubeBaseActivity", link:"reference/com/google/android/youtube/player/YouTubeBaseActivity.html", type:"class", deprecated:"false" },
+      { id:3, label:"com.google.android.youtube.player.YouTubeInitializationResult", link:"reference/com/google/android/youtube/player/YouTubeInitializationResult.html", type:"class", deprecated:"false" },
+      { id:4, label:"com.google.android.youtube.player.YouTubeIntents", link:"reference/com/google/android/youtube/player/YouTubeIntents.html", type:"class", deprecated:"false" },
+      { id:5, label:"com.google.android.youtube.player.YouTubePlayer", link:"reference/com/google/android/youtube/player/YouTubePlayer.html", type:"class", deprecated:"false" },
+      { id:6, label:"com.google.android.youtube.player.YouTubePlayer.ErrorReason", link:"reference/com/google/android/youtube/player/YouTubePlayer.ErrorReason.html", type:"class", deprecated:"false" },
+      { id:7, label:"com.google.android.youtube.player.YouTubePlayer.OnFullscreenListener", link:"reference/com/google/android/youtube/player/YouTubePlayer.OnFullscreenListener.html", type:"class", deprecated:"false" },
+      { id:8, label:"com.google.android.youtube.player.YouTubePlayer.OnInitializedListener", link:"reference/com/google/android/youtube/player/YouTubePlayer.OnInitializedListener.html", type:"class", deprecated:"false" },
+      { id:9, label:"com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener", link:"reference/com/google/android/youtube/player/YouTubePlayer.PlaybackEventListener.html", type:"class", deprecated:"false" },
+      { id:10, label:"com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener", link:"reference/com/google/android/youtube/player/YouTubePlayer.PlayerStateChangeListener.html", type:"class", deprecated:"false" },
+      { id:11, label:"com.google.android.youtube.player.YouTubePlayer.PlayerStyle", link:"reference/com/google/android/youtube/player/YouTubePlayer.PlayerStyle.html", type:"class", deprecated:"false" },
+      { id:12, label:"com.google.android.youtube.player.YouTubePlayer.PlaylistEventListener", link:"reference/com/google/android/youtube/player/YouTubePlayer.PlaylistEventListener.html", type:"class", deprecated:"false" },
+      { id:13, label:"com.google.android.youtube.player.YouTubePlayer.Provider", link:"reference/com/google/android/youtube/player/YouTubePlayer.Provider.html", type:"class", deprecated:"false" },
+      { id:14, label:"com.google.android.youtube.player.YouTubePlayerFragment", link:"reference/com/google/android/youtube/player/YouTubePlayerFragment.html", type:"class", deprecated:"false" },
+      { id:15, label:"com.google.android.youtube.player.YouTubePlayerSupportFragment", link:"reference/com/google/android/youtube/player/YouTubePlayerSupportFragment.html", type:"class", deprecated:"false" },
+      { id:16, label:"com.google.android.youtube.player.YouTubePlayerView", link:"reference/com/google/android/youtube/player/YouTubePlayerView.html", type:"class", deprecated:"false" },
+      { id:17, label:"com.google.android.youtube.player.YouTubeStandalonePlayer", link:"reference/com/google/android/youtube/player/YouTubeStandalonePlayer.html", type:"class", deprecated:"false" },
+      { id:18, label:"com.google.android.youtube.player.YouTubeThumbnailLoader", link:"reference/com/google/android/youtube/player/YouTubeThumbnailLoader.html", type:"class", deprecated:"false" },
+      { id:19, label:"com.google.android.youtube.player.YouTubeThumbnailLoader.ErrorReason", link:"reference/com/google/android/youtube/player/YouTubeThumbnailLoader.ErrorReason.html", type:"class", deprecated:"false" },
+      { id:20, label:"com.google.android.youtube.player.YouTubeThumbnailLoader.OnThumbnailLoadedListener", link:"reference/com/google/android/youtube/player/YouTubeThumbnailLoader.OnThumbnailLoadedListener.html", type:"class", deprecated:"false" },
+      { id:21, label:"com.google.android.youtube.player.YouTubeThumbnailView", link:"reference/com/google/android/youtube/player/YouTubeThumbnailView.html", type:"class", deprecated:"false" },
+      { id:22, label:"com.google.android.youtube.player.YouTubeThumbnailView.OnInitializedListener", link:"reference/com/google/android/youtube/player/YouTubeThumbnailView.OnInitializedListener.html", type:"class", deprecated:"false" }
+
+    ];
